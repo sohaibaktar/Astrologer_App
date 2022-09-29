b36a4f520bba530633de845565580f0c5a13c59b
Sohaib Aktar Gazi <aktar19sohaib@gmail.com>
2022-09-30 01:02:54 +0530

    some chat app changes

--- app/src/main/java/com/b/chattappmean/Activities/PhoneNumberActivity.java
+++ app/src/main/java/com/b/chattappmean/Activities/PhoneNumberActivity.java
@@ -1,18 +1,29 @@
 package com.b.chattappmean.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.b.chattappmean.R;
 import com.b.chattappmean.databinding.ActivityPhoneNumberBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class PhoneNumberActivity extends AppCompatActivity {
 
     ActivityPhoneNumberBinding binding;
-    FirebaseAuth auth;
+
+    private FirebaseAuth mAuth;
+    private EditText email_,password_;
 
 
     @Override
@@ -20,10 +31,10 @@ public class PhoneNumberActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        auth = FirebaseAuth.getInstance();
-
-        if(auth.getCurrentUser() != null) {
+        email_ = findViewById(R.id.mail_id);
+        password_ = findViewById(R.id.mail_pass);
+        mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser() != null) {
             Intent intent = new Intent(PhoneNumberActivity.this,MainActivity.class);
             startActivity(intent);
             finish();
@@ -43,5 +54,39 @@ public class PhoneNumberActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            //reload();
+        }
+    }
+
+    public void SignUp(View view) {
+        String email = email_.getText().toString();
+        String password = password_.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithEmail:success");
+                            Intent intent = new Intent(PhoneNumberActivity.this, SetupProfileActivity.class);
+                            startActivity(intent);
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(PhoneNumberActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/b/chattappmean/Activities/Registration.java
@@ -0,0 +1,14 @@
+package com.b.chattappmean.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
